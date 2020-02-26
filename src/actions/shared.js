ece5bd6bff97b4ec452ed1cba40143287dd8c9a2
İlker Bağcı <ilkerbagci0@gmail.com>
2020-02-26 23:01:33 +0300

    Toggle the tweet like

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,7 +1,7 @@
-import { getInitialData } from "../utils/api"
-import { receiveUsers } from "./users"
-import { receiveTweets } from "./tweets"
-import { setAuthedUser } from "./authedUser"
+import { getInitialData } from '../utils/api'
+import { receiveUsers } from './users'
+import { receiveTweets } from './tweets'
+import { setAuthedUser } from './authedUser'
 import { showLoading, hideLoading } from 'react-redux-loading'
 
 const AUTHED_ID = 'tylermcginnis'
--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -4,6 +4,7 @@ import { formatTweet, formatDate } from '../utils/helpers'
 import { TiArrowBackOutline } from 'react-icons/ti/index'
 import { TiHeartOutline } from 'react-icons/ti/index'
 import { TiHeartFullOutline } from 'react-icons/ti/index'
+import { handleToggleTweet } from '../actions/tweets'
 
 class Tweet extends React.Component {
     toParent = (e, id) => {
@@ -13,7 +14,14 @@ class Tweet extends React.Component {
 
     handleLike = e => {
         e.preventDefault()
-        //TODO: Handle like tweet
+        
+        const { dispatch, tweet, authedUser } = this.props
+        
+        dispatch(handleToggleTweet({
+            id: tweet.id,
+            hasLiked: tweet.hasLiked,
+            authedUser
+        }))
     }
 
     render() {
